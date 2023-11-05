8ccfef25d02381354ae8b5ea802e76e9e4bcf203
Kattegatt <kostenkodenys30@gmail.com>
2023-11-05 19:20:55 +0200

    connect search component

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,20 @@
 	<div class="flex flex-col items-center justify-center">
 		<input-form @form-route="addNewRoute" />
 	</div>
-	<search-bar />
+	<search-bar @activeInput="updateSearchInput" />
+	<div class="flex">
+		<button
+			class="flex items-center justify-center mx-2 px-3 h-8 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700"
+		>
+			Previous
+		</button>
+
+		<button
+			class="flex items-center justify-center px-3 h-8 ml-3 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700"
+		>
+			Next
+		</button>
+	</div>
 	<training-history :training-days-list="trainingDaysList" @deleteBtnPressed="removeRoute" />
 </template>
 
@@ -16,6 +29,8 @@ import trainingHistory from './components/trainingHistory.vue';
 export default {
 	data() {
 		return {
+			currentPage: 1,
+			searchInput: '',
 			trainingDaysList: [],
 			routes: [],
 		};
@@ -25,8 +40,15 @@ export default {
 		searchBar,
 		trainingHistory,
 	},
-	computed: {},
+	computed: {
+		paginatedTrainingDaysList() {},
+		calculatedPage() {},
+		lastPage() {},
+	},
 	methods: {
+		updateSearchInput(input) {
+			this.searchInput = input;
+		},
 		addNewRoute(route) {
 			const existingRoute = this.routes.find(
 				r => r.date === route.date && r.name === route.name && r.rating === route.rating
@@ -38,30 +60,31 @@ export default {
 			} else {
 				this.routes.push(route);
 			}
-
-			this.updateTrainingDaysList();
 		},
 		removeRoute(route) {
 			this.routes = this.routes.filter(r => {
-				r !== route;
+				return r !== route;
 			});
-
-			this.updateTrainingDaysList();
-		},
-		updateTrainingDaysList() {
-			let routes = this.routes;
-			let uniqueDates = [...new Set(routes.map(r => r.date))];
-
-			let trainingDaysList = uniqueDates.map(date => {
-				let filteredRoutes = routes.filter(r => r.date === date);
-				return { date: date, routes: filteredRoutes };
-			});
-
-			this.trainingDaysList = trainingDaysList;
 		},
 	},
-	// watch: {
-	// 	routes() {},
-	// },
+	watch: {
+		routes: {
+			handler() {
+				let routes = this.routes;
+				let uniqueDates = [...new Set(routes.map(r => r.date))];
+				let trainingDaysList = uniqueDates.map(date => {
+					let filteredRoutes = routes.filter(r => r.date === date);
+					return { date: date, routes: filteredRoutes };
+				});
+				trainingDaysList.sort((a, b) => {
+					let aDate = new Date(a.date);
+					let bDate = new Date(b.date);
+					return aDate - bDate;
+				});
+				this.trainingDaysList = trainingDaysList;
+			},
+			deep: true,
+		},
+	},
 };
 </script>
--- src/components/searchBar.vue
+++ src/components/searchBar.vue
@@ -1,30 +1,30 @@
 <template>
-	<form class="m-2 flex items-center w-96">
-		<label for="simple-search" class="sr-only">Search</label>
+	<form class="m-2 flex items-center w-40">
 		<div class="relative w-full">
 			<div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"></div>
 			<input
+				v-model="searchInput"
 				type="text"
 				id="simple-search"
-				class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-black focus:border-gray-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-				placeholder="Search branch name..."
-				required
+				class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-black focus:border-gray-500 block w-full pl-10 p-2.5"
+				placeholder="Search"
+				@input="emitInput"
 			/>
 		</div>
-		<button
-			type="submit"
-			class="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-		>
-			<span class="sr-only bg-black">Search</span>
-		</button>
 	</form>
 </template>
 
 <script>
 export default {
 	data() {
-		return {};
+		return {
+			searchInput: '',
+		};
 	},
-	methods: {},
+	methods: {
+		emitInput() {
+			this.$emit('activeInput', this.searchInput);
+		},
+	},
 };
 </script>
