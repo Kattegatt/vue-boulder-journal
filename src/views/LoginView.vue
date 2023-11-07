67b1292c6f103c3a78fc1a21436db724d5081492
Kattegatt <kostenkodenys30@gmail.com>
2023-11-07 23:25:21 +0200

    basic routing added

---
+++ src/views/LoginView.vue
@@ -0,0 +1,9 @@
+<template>
+	<div class="my-10 flex justify-center">Im Login View</div>
+	<div class="flex justify-center">
+		<br />
+		<a href="/">Home View</a>
+	</div>
+</template>
+
+<script></script>
